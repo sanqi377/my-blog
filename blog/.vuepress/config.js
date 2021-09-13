27f4a3bafa406dc8c2fc0d0432ae63556e2c1d91
sanqi377 <sanqi377@outlook.com>
2021-09-13 13:16:35 +0800

    first commit

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Im\'s Sanqi',
+  description: '坚持是对平庸最好的打击！',
+  port: 8088,
+  markdown: {
+    lineNumbers: true,
+    extractHeaders: [ 'h2', 'h3', 'h4' ],
+    plugins: {
+      'markdown-it-mark': true,
+      'markdown-it-footnote': true,
+      'markdown-it-abbr': true,
+      'markdown-it-task-lists': true
+    }
+  },
+  theme: require.resolve('../../index'), // 使用本地主题
+  themeConfig: require('./config/themeConfig')
+}
